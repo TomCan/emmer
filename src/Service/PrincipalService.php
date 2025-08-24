a101669e1567c52f9188d392510f1cdbd0dab6ef
Tom Cannaerts <tom.cannaerts@iodigital.com>
2025-08-24 11:48:45 +0200

    Don't add ROOT

--- src/Service/PrincipalService.php
+++ src/Service/PrincipalService.php
@@ -17,9 +17,9 @@ class PrincipalService
             $principals[] = 'emr:user:@anonymous';
         } else {
             $principals[] = $user->getIdentifier();
-            // add roles, but note ROLE_USER as every user has this role
+            // add roles, but note USER as every user has this role, nor ROOT as this is treated seperately
             foreach ($user->getRoles() as $role) {
-                if ('ROLE_USER' !== $role) {
+                if ('USER' !== $role && 'ROOT' !== $role) {
                     if (str_starts_with($role, 'ROLE_')) {
                         $role = strtolower(substr($role, 5));
                     } else {
