4d22bbdee054a35d9d4d019891adb161d3e56f15
Tom Cannaerts <tom.cannaerts@iodigital.com>
2025-10-01 23:33:45 +0200

    Dockerfile + entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-LISTEN=${LISTEN:-"0.0.0.0:8080"}
+if [ "$1" != "" ]
+then
+    exec "$@"
+else
+    if [ "${INIT_KEY}" != "" ] && [ "${INIT_SECRET}" != "" ]
+    then
+        X=$(bin/console doctrine:schema:create)
+        if [ $? -eq 0 ]
+        then
+            # schema create will fail if it already exists
+            bin/console app:user:create -r ${INIT_USER:-emmer}
+            bin/console app:user:create-access-key --access-key="${INIT_KEY}" --access-secret="${INIT_SECRET}" ${INIT_USER:-emmer}
+        fi
+    fi
 
-php -S $LISTEN -t public public/router.php
+    LISTEN=${LISTEN:-"0.0.0.0:8080"}
+    exec php -S $LISTEN -t public public/router.php
+fi
