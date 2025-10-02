a170950aefc49e3cd2359e9cd7dd3e918dea41da
Tom Cannaerts <tom.cannaerts@iodigital.com>
2025-10-03 00:34:14 +0200

    codestyles

--- public/router.php
+++ public/router.php
@@ -2,15 +2,13 @@
 
 // router.php - Router file for Symfony with PHP built-in web server
 
-use Symfony\Component\HttpFoundation\Request;
-
 // Check if the requested file is a real file and serve it directly
-if (is_file($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . $_SERVER['REQUEST_URI'])) {
+if (is_file($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.$_SERVER['REQUEST_URI'])) {
     return false;
 }
 
 // Set the script name to the front controller
-$_SERVER['SCRIPT_FILENAME'] = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'index.php';
+$_SERVER['SCRIPT_FILENAME'] = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'index.php';
 
 // Include the Symfony front controller
-require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 'index.php';
+require_once $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'index.php';
